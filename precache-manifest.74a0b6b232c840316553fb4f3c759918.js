5b697ede8f77878a0bce8b6903c7f704c484cac7
jesseilev <jesseilev@gmail.com>
2019-03-07 23:56:16 -0500

    Updates

---
+++ precache-manifest.74a0b6b232c840316553fb4f3c759918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "878129fb7eaf3d9278f7",
+    "url": "/fang-fetcher-redux/static/js/runtime~main.878129fb.js"
+  },
+  {
+    "revision": "512056531256472fad97",
+    "url": "/fang-fetcher-redux/static/js/main.51205653.chunk.js"
+  },
+  {
+    "revision": "60e79615170fc3a796cd",
+    "url": "/fang-fetcher-redux/static/js/2.60e79615.chunk.js"
+  },
+  {
+    "revision": "512056531256472fad97",
+    "url": "/fang-fetcher-redux/static/css/main.65ea91df.chunk.css"
+  },
+  {
+    "revision": "60e79615170fc3a796cd",
+    "url": "/fang-fetcher-redux/static/css/2.33ab0548.chunk.css"
+  },
+  {
+    "revision": "e863c5c0c479c5e0067cc675236d1235",
+    "url": "/fang-fetcher-redux/index.html"
+  }
+];
